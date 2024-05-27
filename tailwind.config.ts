852b82ccc41c655c8d2da3abef55eaf2667f9fdc
“daryuun5139” <118511364+daryuun5139@users.noreply.github.com>
2024-05-27 23:15:20 +0900

    header作成

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      content: {
+        search: 'url("./search.svg")',
+      },
+    },
   },
   plugins: [],
 } as Config;
